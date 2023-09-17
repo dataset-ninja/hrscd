8adf61290d676a1f94d419d524a708a66517abc5
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-17 14:08:31 +0600

    DOWNLOAD_ORIGINAL_URL

--- src/settings.py
+++ src/settings.py
@@ -43,7 +43,7 @@ GITHUB_URL: str = None
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/javidtheimmortal/high-resolution-semantic-change-detection-dataset/download?datasetVersionNumber=1"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
